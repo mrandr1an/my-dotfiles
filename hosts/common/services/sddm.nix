b502c89890142cf7889401711c366028df37360c
mrandr1an <krackedissad@gmail.com>
2025-07-27 17:11:59 +0300

    Move embeddedTheme to the correct scope

--- hosts/common/services/sddm.nix
+++ hosts/common/services/sddm.nix
@@ -5,18 +5,21 @@ let
        AccentColor = "#746385";
        FormPosition = "left";
        ForceHideCompletePassword = true; 
-       embeddedTheme = "japanese_aesthetic";
-      };
+      }; 
+      embeddedTheme = "japanese_aesthetic";
    };
 in
 {
    #Enable and configure the SDDM display manager
    services.displayManager.sddm.enable = true;  
-   #Set Theme
    services.displayManager.sddm.theme = "sddm-astronaut-theme";
    services.displayManager.sddm.package = pkgs.kdePackages.sddm;
    services.displayManager.sddm.wayland.enable = true; 
+   services.displayManager.sddm.enableHidpi = true;
    services.displayManager.sddm.extraPackages = [sddm-astronaut]; 
    
-  environment.systemPackages = [sddm-astronaut];
+   environment.systemPackages = [
+             sddm-astronaut
+             pkgs.kdePackages.qtmultimedia
+            ];
 }
