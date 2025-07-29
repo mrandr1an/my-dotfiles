2eaccef36b67fa12061e75557c309ced51f53176
mrandr1an <krackedissad@gmail.com>
2025-07-29 15:16:39 +0300

    Add secrets for ssh keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,11 @@
 let
-  github_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJrjbmxHd4yjq5Ge/vw3J1BYSjc9SngURx5a9kH+jGnW";
+  workstation_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJrjbmxHd4yjq5Ge/vw3J1BYSjc9SngURx5a9kH+jGnW";
+  laptop_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjdsvQ5Bd0kXFAjVhD/VY1vi/fFERnUYHo6U7DcN4RX"; 
 in
 {
-  "secret1.age".publicKeys = [ github_key ];
-  "secret2.age".publicKeys = [ github_key ];
+  "id_github_laptop.age".publicKeys = [ laptop_key ];
+  "id_github_laptop_pub.age".publicKeys = [ laptop_key ];
+  "id_github_workstation.age".publicKeys = [ workstation_key ];
+  "id_github_workstation_pub.age".publicKeys = [ workstation_key ];
 }
 
