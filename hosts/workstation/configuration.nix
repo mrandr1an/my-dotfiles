7dc3d8d00fb580b82f10280ddcf37ab10b6da349
mrandr1an <krackedissad@gmail.com>
2025-07-30 16:04:30 +0300

    implement system level identinties

--- hosts/workstation/configuration.nix
+++ hosts/workstation/configuration.nix
@@ -158,12 +158,14 @@
     file = ../../secrets/id_github_workstation.age;
     owner = "chrisl";
     mode = "0400";
+    path = "/home/chrisl/.ssh/id_github_workstation";
   };
 
   age.secrets.id_github_workstation_pub = {
     file = ../../secrets/id_github_workstation_pub.age;
     owner = "chrisl";
     mode = "0400";
+    path = "/home/chrisl/.ssh/id_github_workstation.pub";
   };
   
   # Open ports in the firewall.
--- hosts/workstation/users/chrisl-home.nix
+++ hosts/workstation/users/chrisl-home.nix
@@ -52,14 +52,4 @@ home.file.".config/niri/".source  = ../../../dotfiles/niri;
    pkgs.ansible_2_17
  ];
 
- #Configure identities
-home.file.".ssh/id_github_workstation" = {
-  source = /run/agenix/id_github_workstation;
-  permissions = "0600";
-};
-
-home.file.".ssh/id_github_workstation.pub" = {
-  source = /run/agenix/id_github_workstation_pub;
-  permissions = "0644";
-};
 }
