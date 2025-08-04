8788aa04695b7addff6f9cb02a660599ef01fc35
mrandr1an <krackedissad@gmail.com>
2025-08-04 19:42:09 +0300

    changed name of host to laptop

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,7 +1,7 @@
 {inputs,lib,...}: 
 
 {
- flake.nixosConfigurations.workstation = inputs.nixpkgs.lib.nixosSystem {
+ flake.nixosConfigurations.laptop = inputs.nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   modules = [
       ./configuration.nix
