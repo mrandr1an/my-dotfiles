a7e4d7b16cb1cebbbcbb79c5d3782581f5bf7de7
mrandr1an <krackedissad@gmail.com>
2025-08-05 23:09:09 +0300

    settling on waybar for now

--- flake.nix
+++ flake.nix
@@ -4,11 +4,6 @@
   inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
-    	quickshell = {
-      	 url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-      	 inputs.nixpkgs.follows = "nixpkgs";
-    	}; 
-
         home-manager = {
           url = "github:nix-community/home-manager/release-25.05";
 	  inputs.nixpkgs.follows = "nixpkgs";
