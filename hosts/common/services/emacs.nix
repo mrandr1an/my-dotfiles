1014b70884e495442a8758bf9b9d829b3cdf84e9
mrandr1an <krackedissad@gmail.com>
2025-08-09 15:10:01 -0400

    try to load emacs config

--- hosts/common/services/emacs.nix
+++ hosts/common/services/emacs.nix
@@ -1,14 +1,29 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
+let
+  # Choose the Emacs you want:
+  #   pkgs.emacs         (GTK)
+  #   pkgs.emacsPgtk     (PGTK)
+  #   pkgs.emacs-unstable (from emacs-overlay, if you wired it)
+  baseEmacs = pkgs.emacs;
+
+  emacsPkgs = pkgs.emacsPackagesFor baseEmacs;
+in
 {
   environment.systemPackages = [
-    (pkgs.emacsWithPackagesFromUsePackage {
-      package = pkgs.emacsGit;  # replace with pkgs.emacsPgtk, or another version if desired.
-      config = "../../../dotfiles/emacs/";
+    (emacsPkgs.emacsWithPackagesFromUsePackage {
+      package = emacsPkgs.emacs;     # ← use the emacs from this package set
+      config  = ../../../dotfiles/emacs/init.el;            # ← stable path; keep it near the module
 
-      extraEmacsPackages = epkgs: [
-        epkgs.use-package;
+      extraEmacsPackages = epkgs: with epkgs; [
+        use-package
+        # add packages that your init.el requires but can't infer
+        # e.g. magit org-contrib consult orderless vertico
       ];
 
+      # Optional helpers:
+      # alwaysEnsure = true;  # treat :ensure as true by default
+      # alwaysTangle = true;  # if you keep org-babel configs
     })
   ];
- }
+}
+
