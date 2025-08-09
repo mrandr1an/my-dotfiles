1ccd866e118c5193d413f68f52bbb4b0f072a4b2
mrandr1an <krackedissad@gmail.com>
2025-08-09 15:00:40 -0400

    path into string

--- hosts/common/services/emacs.nix
+++ hosts/common/services/emacs.nix
@@ -3,7 +3,7 @@
   environment.systemPackages = [
     (pkgs.emacsWithPackagesFromUsePackage {
       package = pkgs.emacsGit;  # replace with pkgs.emacsPgtk, or another version if desired.
-      config = ../../../dotfiles/emacs/;
+      config = "../../../dotfiles/emacs/";
 
       extraEmacsPackages = epkgs: [
         epkgs.use-package;
