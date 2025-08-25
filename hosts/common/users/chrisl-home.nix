c55ca348cbf74ae4037b816a444d9d8956ea36f6
mrandr1an <krackedissad@gmail.com>
2025-08-25 13:22:17 -0400

    attempt to use manually written emacs module

--- hosts/common/users/chrisl-home.nix
+++ hosts/common/users/chrisl-home.nix
@@ -1,28 +1,20 @@
 {config, pkgs, ...} :
-let 
-emacsOverlay = 
-    (import (builtins.fetchTarball {
-      url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-      sha256 = "sha256-+B/GngvxzeLxe5JXuJkXNc3k77jLiyb0p3rwvu5hLPA=";
-    }));
-pkgsEmacs = import pkgs.path {
- system = pkgs.system;
- overlays = [ emacsOverlay ];
- config = pkgs.config;
-};
-myEmacs = pkgsEmacs.emacsWithPackagesFromUsePackage {
-  package = pkgsEmacs.emacs-unstable-pgtk.override {
-    withTreeSitter = true;
-  };
- config = builtins.readFile ../../../dotfiles/emacs/init.el;
- extraEmacsPackages = epkgs: [
-   epkgs.use-package
-   epkgs.treesit-grammars.with-all-grammars
- ];
-};
-in
 {
+  imports = [
+    ../../../hm/modules/emacs.nix
+  ];
 
+  apps.emacs =  {
+    enable = true;
+    overlay.enable = true;
+    package = {
+      variant = "pgtk";
+      withTreeSitter = true; 
+    };
+    service.enable = true;
+    dotfiles.enable = true;
+  };
+    
 home.username = "chrisl";
 home.homeDirectory = "/home/chrisl";
 home.stateVersion = "25.05";
@@ -41,20 +33,6 @@ services.syncthing = {
   enable = true;
  };
 
-services.emacs = {
- enable = true;
- package = myEmacs;
- client.enable = true;
- socketActivation.enable = true;
- defaultEditor = true;
- startWithUserSession = true;
-};
-
-programs.emacs = {
-  enable = true;                                 
-  package = myEmacs;
-};
-
 programs = {
   direnv = {
     enable = true;
@@ -67,10 +45,6 @@ programs = {
 home.file.".config/niri/".source = ../../../dotfiles/niri;
 home.file.".config/waybar/".source = ../../../dotfiles/waybar;
 home.file.".config/quickshell/".source = ../../../dotfiles/quickshell;
-home.file.".emacs.d" = {
- source = config.lib.file.mkOutOfStoreSymlink /home/chrisl/.dotfiles/dotfiles/emacs; 
- force = true;
-};
   
 #Home Packages
 home.packages = with pkgs;
