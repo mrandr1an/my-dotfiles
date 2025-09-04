00a84b5073829389f781d16cf8cd01ebd69e90dc
mrandr1an <krackedissad@gmail.com>
2025-09-04 03:30:52 -0400

    formatted a bit

--- flake.nix
+++ flake.nix
@@ -1,28 +1,30 @@
 {
   description = "My first flake";
+
   inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
         home-manager = {
           url = "github:nix-community/home-manager/release-25.05";
-	  inputs.nixpkgs.follows = "nixpkgs";
+	        inputs.nixpkgs.follows = "nixpkgs";
         };
 	
- 	agenix = {
+ 	      agenix = {
           url = "github:ryantm/agenix";
           inputs.nixpkgs.follows = "nixpkgs"; 
-	};
+	      };
        
         flake-parts = {
- 	 url = "github:hercules-ci/flake-parts";
-	};
+ 	        url = "github:hercules-ci/flake-parts";
+	      };
   };
 
   outputs = inputs@{self,flake-parts,...}:
    flake-parts.lib.mkFlake { inherit inputs; } (top@{config,withSystem,moduleWithSystem,...}:
    {
      imports = [
-		./hosts/workstation
-		./hosts/laptop
+		   ./hosts/workstation
+		   ./hosts/laptop 
     	       ];
 
      flake = {};
