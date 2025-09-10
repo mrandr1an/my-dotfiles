813f1b33b0232ee788bc01ee8d5be46e90f426a2
mrandr1an <krackedissad@gmail.com>
2025-09-10 15:45:33 +0300

    update waybar

--- dotfiles/waybar/scripts/audio.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2 * 100)}')
-mute=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q MUTED && echo " (muted)" || echo "")
-echo "${vol}%${mute}"
