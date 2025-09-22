b32de204cb1c210c78fd5408c64475ea313fc8a9
mrandr1an <krackedissad@gmail.com>
2025-09-22 15:02:31 +0300

    use niri-flake as a homeModule

--- lib/mkSystem.nix
+++ lib/mkSystem.nix
@@ -5,10 +5,10 @@ inputs.nixpkgs.lib.nixosSystem {
   system = "x86-64-linux";
   specialArgs = { inherit inputs archetype; };
   modules = [
-    inputs.niri-flake.nixosModules.niri
+    inputs.disko.nixosModules.disko
     inputs.agenix.nixosModules.age
     inputs.home-manager.nixosModules.home-manager
-    inputs.disko.nixosModules.disko
+    { home-manager.sharedModules = [ inputs.niri-flake.homeModules.niri ]; }
     (import ../disko/laptop-btrfs-encrypted.nix {
           hdd = { name = "/dev/sdb"; };
           ssd = { name = "/dev/sda"; };
