caec1868f67aac9fb38edd2f16ae5f520fbdc0f9
mrandr1an <krackedissad@gmail.com>
2025-09-30 21:21:07 +0300

    fix(lib/modules/system/secrets.nix): Change path.

--- lib/modules/system/secrets.nix
+++ lib/modules/system/secrets.nix
@@ -15,24 +15,24 @@ in
     age.identityPaths = cfg.privateKeyPaths;
 
     age.secrets.id_github_workstation = {
-      file = ../../secrets/id_github_workstation.age;
+      file = ../../../secrets/id_github_workstation.age;
       owner = "chrisl";
       mode = "0400";
       path = "/home/chrisl/.ssh/id_github_workstation";
     };
 
     age.secrets.id_github_workstation_pub = {
-      file = ../../secrets/id_github_workstation_pub.age;
+      file = ../../../secrets/id_github_workstation_pub.age;
       owner = "chrisl";
       mode = "0400";
       path = "/home/chrisl/.ssh/id_github_workstation.pub";
     };
 
     age.secrets.chrisl_password = {
-      file = ../../secrets/chrisl_password.age;
+      file = ../../../secrets/chrisl_password.age;
       owner = "chrisl";
       mode = "0400";
     };
   };
-
+  
 }
