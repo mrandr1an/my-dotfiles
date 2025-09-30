965f43d8503c39f8eba70911b4482b7fd8b8d8b4
mrandr1an <krackedissad@gmail.com>
2025-09-30 18:16:57 +0300

    experiment(lib/modules/system/secrets.nix): Add experimental module.
    Very experimental secrets implementation.

---
+++ lib/modules/system/secrets.nix
@@ -0,0 +1,37 @@
+#lib/modules/system/secrets.nix
+{config,lib,archetype,...}:
+let
+  cfg = config.syssecrets;
+in
+{
+  options.syssecrets = {
+    privateKeyPaths = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ "/home/${archetype.user}/.ssh/id_workstation" ];
+    };
+  };
+
+  config = {
+    age.identityPaths = cfg.privateKeyPaths;
+  };
+
+  age.secrets.id_github_workstation = {
+    file = ../../secrets/id_github_workstation.age;
+    owner = "chrisl";
+    mode = "0400";
+    path = "/home/chrisl/.ssh/id_github_workstation";
+  };
+
+  age.secrets.id_github_workstation_pub = {
+    file = ../../secrets/id_github_workstation_pub.age;
+    owner = "chrisl";
+    mode = "0400";
+    path = "/home/chrisl/.ssh/id_github_workstation.pub";
+  };
+
+  age.secrets.email_password = {
+    file = ../../secrets/email_password.age;
+    owner = "chrisl";
+    mode = "0400";
+  };
+}
